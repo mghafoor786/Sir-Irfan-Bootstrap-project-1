755643b103b076593270a90b342e44b6f5b960ad
mghafoor786 <muhammadghafoor659@gmail.com>
2025-11-03 07:29:57 +0500

    wip

--- main.js
+++ main.js
@@ -22,9 +22,9 @@ const swiper = new Swiper(".mySwiper", {
   breakpoints: {
     0: { slidesPerView: 1 },
     576: { slidesPerView: 1 },
-    768: { slidesPerView: 2 },
-    992: { slidesPerView: 3 },
-    1200: { slidesPerView: 4 },
+    // 768: { slidesPerView: 2 },
+    // 992: { slidesPerView: 3 },
+    // 1200: { slidesPerView: 4 },
   },
 });
 
